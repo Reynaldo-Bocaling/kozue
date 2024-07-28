2fe272ab9897602bf0abf9ead15b239ded52b790
Reynaldo <reynaldobocaling@gmail.com>
2024-07-28 17:59:09 +0800

    uplaod proj

--- src/view/Hero.jsx
+++ src/view/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <>
       <Section id="hero">
         <div className="flexCenter flex-cols flex-rows gap-12 mb-32">
-          <div className="relative h-[450px] md:h-[750px] lg:h-[924px] w-full flexCenter">
+          <div className="relative max-w-7xl mx-auto h-[450px] md:h-[750px] lg:h-[924px] w-full flexCenter">
             <div className="relative  z-[98] flexColCenter gap-3 translate-y-44 md:translate-y-56 lg:translate-y-[20rem]">
               <h1 className="title-font text-white text-6xl md:text-9xl font-bold ">
                 $KOZUE
@@ -23,7 +23,7 @@ const Hero = () => {
             <img
               src="/img/hero.png"
               alt=""
-              className="w-[70%] h-[450px] md:h-[900px] lg:h-[1124px]  absolute -bottom-3 left-1/2 -translate-x-1/2 z-1"
+              className="w-full h-[450px] md:h-[900px] lg:h-[1124px]  absolute -bottom-3 left-1/2 -translate-x-1/2 z-1"
             />
           </div>
         </div>
